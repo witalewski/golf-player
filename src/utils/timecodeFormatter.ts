2fe3c121acd7218e0ece786000136bd541912e29
Krzysztof Adam Witalewski <krzysztof.witalewski@gmail.com>
2019-02-04 14:07:28 +0100

    add typescript support

--- src/utils/timecodeFormatter.ts
+++ src/utils/timecodeFormatter.ts
@@ -1,9 +1,11 @@
-const leftPad = number => `${number < 10 ? "0" : ""}${number}`;
+function leftPad(n: number): string {
+  return `${n < 10 ? "0" : ""}${n}`;
+}
 
-export const formatTimecode = time => {
+export function formatTimecode(time: number): string {
   const frames = leftPad(Math.floor((time % 1) / 0.04));
   const seconds = leftPad(Math.floor(time % 60));
   const minutes = leftPad(Math.floor(time / 60));
   const hours = Math.floor(time / (60 * 60));
   return `${hours}:${minutes}:${seconds}:${frames}`;
-};
+}
