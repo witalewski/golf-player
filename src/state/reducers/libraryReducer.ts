4ab1963035042a2fc0841944eb928a6e1c86d83d
Krzysztof Adam Witalewski <krzysztof.witalewski@gmail.com>
2019-02-04 17:29:00 +0100

    refactor sorting movies to a separate file

--- src/state/reducers/libraryReducer.ts
+++ src/state/reducers/libraryReducer.ts
@@ -1,28 +1,11 @@
 import { List } from "immutable";
 import { RECEIVE_DETAILS, SORT } from "../actions/libraryActions";
 import { SortOrder } from "../../global/constants";
+import { sortMovies } from "../../utils/movieSorter";
 
 const initialState = {
   movies: List(),
   sortOrder: SortOrder.RecentlyAdded
-};
-
-const sortMovies = (movies: List<Movie>, sortOrder: SortOrder): List<Movie> => {
-  switch (sortOrder) {
-    case SortOrder.Alphabetically:
-      return movies.sort((m1, m2) => m1.title.localeCompare(m2.title));
-    case SortOrder.Rating:
-      return movies.sort((m1, m2) => m2.imdbRating - m1.imdbRating);
-    case SortOrder.ReleaseDate:
-      return movies.sort(
-        (m1, m2) => m2.releaseDate.getTime() - m1.releaseDate.getTime()
-      );
-    case SortOrder.RecentlyAdded:
-    default:
-      return movies.sort(
-        (m1, m2) => m2.dateModified.getTime() - m1.dateModified.getTime()
-      );
-  }
 };
 
 export const libraryReducer = (
