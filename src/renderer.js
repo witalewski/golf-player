4fb57bf6dc31935d6587de560da778c3c4803afa
Krzysztof Adam Witalewski <krzysztof.witalewski@gmail.com>
2019-02-04 08:19:28 +0100

    remove unused imports

--- src/renderer.js
+++ src/renderer.js
@@ -1,11 +1,9 @@
 const { getMovieDirs } = require("./utils/directoryScanner");
-const { parseDirectoryName } = require("./utils/directoryNameParser");
-const { playMovie } = require("./utils/playerLauncher");
 const { MovieList } = require("./components/MovieList");
 
 getMovieDirs().then(dirs => {
   ReactDOM.render(
-    React.createElement(MovieList, {directoryNames: dirs}, null),
-    document.getElementById('root')
+    React.createElement(MovieList, { directoryNames: dirs }, null),
+    document.getElementById("root")
   );
 });
