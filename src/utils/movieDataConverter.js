abd7f1e3b06687d00a842b814c303a9b783bd201
Krzysztof Adam Witalewski <krzysztof.witalewski@gmail.com>
2019-02-04 08:16:04 +0100

    remove extracting country data from themoviedb

--- src/utils/movieDataConverter.js
+++ src/utils/movieDataConverter.js
@@ -2,11 +2,7 @@ export const getMovieFromDbData = (omdb, theMovieDb) => ({
   title: omdb.Title || theMovieDb.details.title,
   runtime: parseInt(omdb.Runtime || theMovieDb.details.runtime),
   director: omdb.Director,
-  country:
-    omdb.Country ||
-    theMovieDb.details.production_countries
-      .map(country => country.name)
-      .join(", "),
+  country: omdb.Country,
   year: parseInt(omdb.Year || theMovieDb.details.release_date),
   plot: omdb.Plot || theMovieDb.details.overview,
   poster: omdb.Poster || theMovieDb.details.poster_path,
