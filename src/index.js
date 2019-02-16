c9bc2eecd461b7c06f89d03904a879ba47ecd818
Krzysztof Adam Witalewski <krzysztof.witalewski@gmail.com>
2019-02-16 08:32:47 +0100

    refactor loading movies

--- src/index.js
+++ src/index.js
@@ -2,51 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
-import axios from "axios";
 import { MainConnected } from "./components/Main";
-import { receiveDetails } from "./state/actions/libraryActions";
 import { reducers } from "./state/reducers";
 import { getMovieDirs } from "./utils/directoryScanner";
-import { parseDirectoryName } from "./utils/directoryNameParser";
-import { getMovieFromDbData } from "./utils/movieDataConverter";
-import { API_URL, API_KEY } from "global/constants";
+import { DEFAULT_DEPTH_LIMIT } from "./global/constants";
 
 const store = window.hasOwnProperty("__REDUX_DEVTOOLS_EXTENSION__")
   ? createStore(reducers, window["__REDUX_DEVTOOLS_EXTENSION__"]())
   : createStore(reducers);
 
-getMovieDirs().then(directories => {
-  console.log(directories);
-  directories.forEach(directory => {
-    const { title, year } = parseDirectoryName(directory.directoryName);
-    axios
-      .post(
-        API_URL,
-        JSON.stringify({
-          title: title.replace(/[^a-zA-Z0-9\s]/g, ""),
-          year
-        }),
-        {
-          headers: {
-            "X-Api-Key": API_KEY,
-            "Content-Type": "application/json"
-          }
-        }
-      )
-      .then(({ data: { omdb, theMovieDb } }) => {
-        const movie = {
-          ...getMovieFromDbData(omdb, theMovieDb),
-          ...directory
-        };
-        if (movie.title) {
-          store.dispatch(receiveDetails(movie));
-        } else {
-          console.log("Couldn't find match for", title);
-        }
-      })
-      .catch(console.log);
-  });
-});
+getMovieDirs(DEFAULT_DEPTH_LIMIT, store);
 
 ReactDOM.render(
   <Provider store={store}>
--- src/utils/directoryScanner.ts
+++ src/utils/directoryScanner.ts
@@ -1,8 +1,13 @@
 import os from "os";
 import fs from "fs";
+import axios from "axios";
 import { List } from "immutable";
 import { getUserVolumes } from "./volumesScanner";
-import { DEFAULT_DEPTH_LIMIT } from "global/constants";
+import { DEFAULT_DEPTH_LIMIT } from "../global/constants";
+import { parseDirectoryName } from "./directoryNameParser";
+import { getMovieFromDbData } from "./movieDataConverter";
+import { API_URL, API_KEY } from "../global/constants";
+import { receiveDetails } from "../state/actions/libraryActions";
 
 interface MovieFile {
   filePath: string;
@@ -13,49 +18,73 @@ interface MovieFile {
   directoryPath: string;
 }
 
+const loadMovieDetails = (movieFile, store) => {
+  const { title, year } = parseDirectoryName(movieFile.directoryName);
+  axios
+    .post(
+      API_URL,
+      JSON.stringify({
+        title: title.replace(/[^a-zA-Z0-9\s]/g, ""),
+        year
+      }),
+      {
+        headers: {
+          "X-Api-Key": API_KEY,
+          "Content-Type": "application/json"
+        }
+      }
+    )
+    .then(({ data: { omdb, theMovieDb } }) => {
+      const movie = {
+        ...getMovieFromDbData(omdb, theMovieDb),
+        ...movieFile
+      };
+      if (movie.title) {
+        store.dispatch(receiveDetails(movie));
+      } else {
+        console.log("Couldn't find match for", title);
+      }
+    })
+    .catch(console.log);
+};
+
 export const scanDirectory = (
   path: string,
   directoryName: string,
-  depthLimit: number = DEFAULT_DEPTH_LIMIT
-): MovieFile[] => {
+  depthLimit: number,
+  store
+): void => {
   console.log("Scanning", `${path}/${directoryName}`);
   try {
-    return List(fs.readdirSync(`${path}/${directoryName}`))
-      .map(item => {
-        const filePath = `${path}/${directoryName}/${item}`;
-        const stats: fs.Stats = fs.statSync(filePath);
-        if (stats.isDirectory() && depthLimit > 0) {
-          return scanDirectory(
-            `${path}/${directoryName}`,
-            item,
-            depthLimit - 1
-          );
-        }
-        if (
-          item.match(/(webm|ogg|mp4|avi|mov|flv|wmv|mkv)$/) &&
-          stats.size > 80 * 1024 * 1024
-        ) {
-          return [
-            {
-              filePath,
-              fileSize: stats.size,
-              fileName: item,
-              dateModified: stats.mtime,
-              directoryName,
-              directoryPath: `${path}/${directoryName}`
-            }
-          ];
-        }
-        return [];
-      })
-      .toArray()
-      .reduce((a, b) => [...a, ...b], []);
+    fs.readdirSync(`${path}/${directoryName}`).forEach(item => {
+      const filePath = `${path}/${directoryName}/${item}`;
+      const stats: fs.Stats = fs.statSync(filePath);
+      if (stats.isDirectory() && depthLimit > 0) {
+        scanDirectory(`${path}/${directoryName}`, item, depthLimit - 1, store);
+      }
+      if (
+        item.match(/(webm|ogg|mp4|avi|mov|flv|wmv|mkv)$/) &&
+        stats.size > 80 * 1024 * 1024
+      ) {
+        loadMovieDetails(
+          {
+            filePath,
+            fileSize: stats.size,
+            fileName: item,
+            dateModified: stats.mtime,
+            directoryName,
+            directoryPath: `${path}/${directoryName}`
+          },
+          store
+        );
+      }
+    });
   } catch (e) {
-    return [];
+    return;
   }
 };
 
-export const getMovieDirs = (): Promise<MovieFile[]> =>
+export const getMovieDirs = (depthLimit, store): Promise<MovieFile[]> =>
   new Promise((resolve, reject) =>
     getUserVolumes().then(volumes =>
       resolve(
@@ -64,7 +93,7 @@ export const getMovieDirs = (): Promise<MovieFile[]> =>
           [os.homedir(), "Movies"],
           [os.homedir(), "Downloads"]
         ]
-          .map(([path, name]) => scanDirectory(path, name))
+          .map(([path, name]) => scanDirectory(path, name, depthLimit, store))
           .reduce((a, b) => [...a, ...b], [])
       )
     )
